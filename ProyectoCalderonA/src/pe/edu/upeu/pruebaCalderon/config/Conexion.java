b875f98577eab2f6a28028ba8963ac4d9d29e0a9
anthonycalderon <anthony1011495@gmail.com>
2014-06-26 14:52:46 -0500

    Update Conexion.java

--- ProyectoCalderonA/src/pe/edu/upeu/pruebaCalderon/config/Conexion.java
+++ ProyectoCalderonA/src/pe/edu/upeu/pruebaCalderon/config/Conexion.java
@@ -11,5 +11,22 @@ package pe.edu.upeu.pruebaCalderon.config;
  * @author alum.fial8
  */
 public class Conexion {
+     private static Connection conex=null;
+   private static String url="jdbc:mysql://localhost/bd_visitas";
+   private static String usuario="root";
+   private static String clave="root";
+   
+   
+   public static Connection getConexion()
+   {
+       try{
+           Class.forName("com.mysql.jdbc.Driver");
+       }catch()
+       {
+           
+       }
+       return conex;
+   }
+}
     
 }
